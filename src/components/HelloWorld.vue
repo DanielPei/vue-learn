88cde51df3450fab5bd088c6e56eda3065f6546d
xiaoqiang.pei <xiaoqiang.pei@yiducloud.cn>
2018-12-23 22:00:57 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,16 @@
   <div>
     <div>{{title}}</div>
     <button @click='init_load'>Click Me</button>
+    <div>
+        <h1> I am a simple component </h1>
+        <!-- 使用 Button 组件 -->
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+    </div>
   </div>
 </template>
 
